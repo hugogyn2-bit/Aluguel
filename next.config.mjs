4c880cd21b0f54b0b2fb28618d8e61d14fd6bd6d
hugogyn2-bit <hugo_gyn2@hotmail.com>
2026-01-08 23:42:33 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,14 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      // Coloque SOMENTE origens confiáveis (nada de '*')
       allowedOrigins: [
-        "seudominio.com",
-        "www.seudominio.com",
         "*.vercel.app"
-      ],
-    },
+      ]
+    }
   },
-
-  // reduz chance de estourar memória no build (ajuda muito em CI/Vercel)
   eslint: {
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
